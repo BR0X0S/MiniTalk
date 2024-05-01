bb856cdef38e9cdd46e1873b67237b36cdc684ed
oumondad <oumondad@e1r9p10.1337.ma>
2024-05-01 17:15:29 +0100

    minitalk 89% loading

--- server.c
+++ server.c
@@ -6,14 +6,13 @@
 /*   By: oumondad <oumondad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/27 15:22:15 by oumondad          #+#    #+#             */
-/*   Updated: 2024/04/30 19:53:39 by oumondad         ###   ########.fr       */
+/*   Updated: 2024/05/01 14:00:23 by oumondad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
-#include <stdio.h>
-#include <unistd.h>
-void hhh(int sig)
+
+void morse(int sig)
 {
 	if(sig == SIGUSR1)
 		printf("1\n");
@@ -29,8 +28,8 @@ int	main(void)
 	data.id = ft_itoa(data.pid);
 	write(1, data.id, ft_strlen(data.id));
 	write(1, "\n", 1);
-	signal(SIGUSR1,hhh);
-	signal(SIGUSR2,hhh);
+	signal(SIGUSR1,morse);
+	signal(SIGUSR2,morse);
 	while(1);
 	return (0);
 }
